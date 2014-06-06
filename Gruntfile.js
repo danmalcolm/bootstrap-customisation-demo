3d8dcf0a9977ace3dc5e4d6d0008b3251885f15e
danmalcolm <danmalc@gmail.com>
2014-06-06 14:27:24 +0100

    Simple example

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+/*!
+  */
+
+module.exports = function (grunt) {
+  'use strict';
+
+  var fs = require('fs');
+  var path = require('path');
+  
+  // Project configuration.
+  grunt.initConfig({
+
+    themePath: 'mountaingoat/',
+	bootStrapPath: '../bootstrap/',
+    
+    // Task configuration.	
+
+	clean: {
+      after: ['<%=bootStrapPath%>less/bak']
+    },
+	
+    copy: {
+      backup: {
+		expand: true,
+		flatten: true,
+		src: '<%= bootStrapPath %>less/variables.less',
+		dest: 'bak/less/'
+	  },
+	  customise: {
+	    expand: true,
+		flatten: true,
+		src: '<%= themePath %>less/variables.less',
+        dest: '<%= bootStrapPath %>less/'
+	  },
+	  restore: {
+		expand: true,
+		flatten: true,
+        src: 'bak/less/variables.less',
+        dest: '<%=bootStrapPath%>/less/'
+	  }
+    },
+	
+	shell: {
+	  buildBootstrap: {
+		command: 'grunt dist',
+		options: {
+		  stdout: true,
+		  execOptions: {
+			cwd: '<%=bootStrapPath%>'
+		  }
+		}
+	  }
+	}	
+  });
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+  require('time-grunt')(grunt);
+
+  grunt.registerTask('default', ['copy:backup', 'copy:customise', 'shell:buildBootstrap', 'copy:restore' ]);
+};
